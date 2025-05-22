cffacabc866543a55fe2fa61eda338d672a3164b
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2025-05-22 05:46:52 +0200

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,11 @@
-const path = require("path");
-
 let mix = require("laravel-mix");
+let NovaExtension = require("laravel-nova-devtool");
 
-require("./nova.mix");
+mix.extend("nova", new NovaExtension());
 
 mix.setPublicPath("dist")
     .js("resources/js/field.js", "js")
     .vue({ version: 3 })
-    .sass('resources/sass/field.scss', 'dist/css')
-    .nova("gabrielesbaiz/nova-field-map");
-
-mix.webpackConfig({
-    resolve: {
-        alias: {
-            "laravel-nova": path.resolve(
-                __dirname,
-                "vendor/laravel/nova/resources/js/mixins/packages.js"
-            ),
-        },
-    },
-});
+    .sass("resources/sass/field.scss", "dist/css")
+    .nova("gabrielesbaiz/nova-field-map")
+    .version();
